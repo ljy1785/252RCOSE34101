3bc440528558182fd0e105fc647dd37ca77d64f1
ljy1785 <ljy1785>
2025-12-04 18:52:54 +0900

    Add array sum example

---
+++ practice/array_sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int sum = 0;
+    for (int i = 0; i < 5; i++) sum += arr[i];
+    printf("%d\n", sum);
+    return 0;
+}
